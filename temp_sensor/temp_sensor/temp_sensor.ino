559d9e6750a5d9ccc2ad73f0f723bdf27d5e73aa
DVNLO <31555490+DVNLO@users.noreply.github.com>
2017-09-29 14:48:45 -0700

    Next project

---
+++ temp_sensor/temp_sensor/temp_sensor.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		temp_sensor.ino
+ Created:	9/28/2017 1:26:10 PM
+ Author:	Daniel Lee Vyenielo
+*/
+
+
+
+void setup() {
+	pinMode()
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
